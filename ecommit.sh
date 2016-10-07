7651e8f29f1db335c15ea2ffab32b6a153fd2540
Daniel Habib <d.g.habib7@gmail.com>
2016-10-07 13:05:17 -0400

    'First Succesful Random Emoji?? => ' :fire:

--- ecommit.sh
+++ ecommit.sh
@@ -1,6 +1,8 @@
 function ecommit {
-    $emoji=$(gshuf -n 1 "emoji.txt")
-    git commit -m "$1 $emoji"
-
+    emoji=$(gshuf -n 1 "emoji.txt")
+    echo $emoji
+    echo "$*"
+    git commit -m "'$*' $emoji"
 }
 
+ecommit "$*"
