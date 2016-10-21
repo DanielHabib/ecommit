88f3286c4d3bf665ac39d283e36a4ddf13f467d2
Daniel Habib <d.g.habib7@gmail.com>
2016-10-21 15:27:08 -0400

    Miracle :bug:

--- ecommit.sh
+++ ecommit.sh
@@ -1,15 +1,68 @@
 function menu {
+emoji=""
 PS3="Please choose an option "
-select option in go stay wait quit
+select option in InitialCommit VersionTag Feature Bugfix Metadata Refactor Documentation Internationalization Performance Cosmetic Tooling Tests Deprecation WIP;
 do
     case $option in
-        go) 
-            echo "Going";;
+        InitialCommit) 
+            emoji=":tada:"
+            break;;
+        VersionTag)
+            emoji=":bookmark:"
+            break;;
+        Feature)
+            emoji=":sparkels:"
+            break;;
+        Bugfix)
+            emoji=":bug:"
+            break;;
+        Metadata)
+            emoji=":card_index:"
+            break;;
+        Refactor)
+            emoji=":package:"
+            break;;
+
+        Documentation)
+            emoji=":books:"
+            break;;
+
+        Internationalization)
+            emoji=":globe_with_meridians:"
+            break;;
+
+        Performance)
+            emoji=":racehorse:"
+            break;;
+
+        Cosmetic)
+            emoji=":lipstick:"
+            break;;
+
+        Tooling)
+            emoji=":wrench:"
+            break;;
+
+        Tests)
+            emoji=":rotating_light:"
+            break;;
+
+        Deprecation)
+            emoji=":poop:"
+            break;;
+
+        WIP)
+            emoji=":construction:"
+            break;;
+
         stay|wait) 
-            echo "Standing by";;
+            echo "Standing by"
+            break;;
+
         quit)
             break;;
- 	esac    
+    esac
+        
 done
 }
 
@@ -19,8 +72,9 @@ function getEmoji {
 
 function ecommit {
     git add .
-    getEmoji
-    emojiCommit=`python3.5 -c "print('$* ' +':{0}:'.format( $emoji))"`
+    # getEmoji
+    menu
+    emojiCommit=`python3.5 -c "print('$* ' +'{0}'.format( '$emoji'))"`
     echo "Generated Emoji: "$emoji
     git commit -m $emojiCommit
 }
